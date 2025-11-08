ba3b2b23bad5f7ae86879da3f3413b461def55b9
KingLemurs <xxdjroundtablexx@gmail.com>
2025-11-07 17:34:12 -0800

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/joshsgyatt/Documents/GitHub/chess-fen-strings
+# Build directory: /Users/joshsgyatt/Documents/GitHub/chess-fen-strings/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
